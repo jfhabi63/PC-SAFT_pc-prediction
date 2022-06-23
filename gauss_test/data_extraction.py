41180208e3a0390091eb5371a278bb471309f062
smjohabi <jonas.habicht@tu-dortmund.de>
2022-06-23 09:25:20 +0200

    Revert "commit"
    This reverts commit ab24a3f2366cc7ac3841c84b26a0aace6e692ed9.

--- gauss_test/data_extraction.py
+++ gauss_test/data_extraction.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import torch
-import numpy as np
 
 plot_kwargs = {"family": 'serif',
                "color": 'r',
@@ -29,175 +27,40 @@ def limits():
     y4 = [-1.02, 0.98]
     return x, y1, y2, y3, y4
 
-def plot_error(err_train, std_train, err_test, std_test, diff_error_train, err_avg_train, diff_error_test, err_avg_test):
-    fig, axs = plt.subplots(1,4, figsize=(30,30))
-    x, y = [0,0.1], [0,0.05]
-    for i in range(4):
-        axs[i].set_box_aspect(1)
-    axs[0].scatter(err_train,std_train,s=9,c='green')
-    axs[0].set_xlabel("error_train",
-                      **plot_kwargs
-                      )
-    axs[0].set_ylabel("std_pred",
-                      **plot_kwargs)
-    axs[1].scatter(err_test,std_test,s=9,c='red')
-    axs[1].set_xlabel("error_test",
-                      **plot_kwargs
-                      )
-    axs[1].set_ylabel("std_pred",
-                      **plot_kwargs)
-    k = int(len(err_train)/2)
-    j = int(len(err_test)/2)
-    try:
-        axs[2].scatter(diff_error_train[0:k], err_avg_train[0:k], s=9, c='green')
-        axs[2].set_xlabel("diff error (train)",
-                          **plot_kwargs
-                          )
-        axs[2].set_ylabel("average prediction error (train)",
-                          **plot_kwargs)
-        axs[3].scatter(diff_error_test[0:j], err_avg_test[0:j], s=9, c='red')
-        axs[3].set_xlabel("diff error (test)",
-                          **plot_kwargs
-                          )
-        axs[3].set_ylabel("average prediction error (test)",
-                          **plot_kwargs)
-    except TypeError:
-        print("asymmetric data, no symmetry error")
-
-
-    for i in range(4):
-        axs[i-1].axis([0,0.01,0,0.01])
-    for i in range(2):
-        axs[i+2].plot(x,y,'g--')
-    plt.savefig('./output/error')
 
 def plot_training(y_train, y_pred_train, y_test, y_pred_test):
-    from Dataset import num_targets
-    fig, axs = plt.subplots(1, num_targets, figsize=(14, 14))
-    plot_names_pred = ["m_seg_pred","sig_pred","eps/k_pred"]
-    plot_names_true = ["m_seg", "sig", "eps/k"]
-
-    for i in range(num_targets):
+    fig, axs = plt.subplots(1, 3, figsize=(14, 14))
+    for i in range(3):
         axs[i].set_box_aspect(1)
-        axs[i].scatter(y_train[:,i], y_pred_train[:,i], s=5, c='green')
-        axs[i].scatter(y_test[:,i], y_pred_test[:,i], s=5, c='red')
-
-        axs[i].set_xlabel(plot_names_true[i],
-                          **plot_kwargs
-                          )
-        axs[i].set_ylabel(plot_names_pred[i],
-                          **plot_kwargs)
-        x, y1, y2, y3, y4 = limits()
-        if num_targets == 1:
-            axs[i].plot(x, y1, 'g--')
-            axs[i].plot(x, y2, 'g--')
-            axs[i].plot(x, x)
-            axs[i].plot(x, y3, 'b--')
-            axs[i].plot(x, y4, 'b--')
-        elif num_targets==3:
-            axs[i].plot([-1000,1000], [-1000,1000])
-    if num_targets ==1:
-        axs[0].axis= ax_limits()
-    elif num_targets==3:
-        axs[0].axis([1,10.5,1,10.5])
-        axs[1].axis([2,5,2,5])
-        axs[2].axis([100,400,100,400])
-    #axs[1].hist2d(y_test,
-    #              y_pred_test,
-    #              **hist_kwargs
-    #           )
-    #axs[2].hist2d(y_train,
-    #              y_pred_train,
-    #              **hist_kwargs
-    #           )
+    axs[0].scatter(y_train, y_pred_train, s=5, c='green')
+    axs[0].scatter(y_test, y_pred_test, s=5, c='red')
+    axs[0].axis(ax_limits())
+    axs[0].set_xlabel("kij_test",
+                      **plot_kwargs
+                      )
+    axs[0].set_ylabel("kij_pred",
+                      **plot_kwargs)
+    x, y1, y2, y3, y4 = limits()
+    axs[0].plot(x, y1, 'g--')
+    axs[0].plot(x, y2, 'g--')
+    axs[0].plot(x, x)
+    axs[0].plot(x, y3, 'b--')
+    axs[0].plot(x, y4, 'b--')
+    axs[1].hist2d(y_test,
+                  y_pred_test,
+                  **hist_kwargs
+               )
+    axs[2].hist2d(y_train,
+                  y_pred_train,
+                  **hist_kwargs
+               )
     plt.savefig('./output/output')
-    plt.close()
 
-def result_export(mean_train,std_train,y_test,error,diff_error, comp, name):
+def result_export(mean_train,std_train,y_test,error, name):
     mean_train = pd.DataFrame(mean_train)
     std_train = pd.DataFrame(std_train)
     y_test = pd.DataFrame(y_test)
-    err_2 = error_threshold(error, 0.02)
-    err_1 = error_threshold(error, 0.01)
-    err_avg = list(np.reshape(np.add(error[0::2],error[1::2])/2,(int(len(error)/2),)))
-    MAE = np.mean(error)
-    MDE = np.mean(diff_error)
-    DE_max = np.max(diff_error)
-    err_stat = [err_2,err_1,MAE,MDE,DE_max]
-    for i in range(len(error)-len(err_stat)):
-        err_stat.append('')
-    comp_half = comp[::2]
-    for i in range(len(error)-len(diff_error)):
-        err_avg.append('')
-        diff_error.append('')
-        comp_half.append('')
     error = pd.DataFrame(error)
-    comp = pd.DataFrame(comp)
-    data=pd.concat([mean_train,std_train,y_test, error,comp],axis=1)
-    data.columns=["kij_pred_µ","kij_pred_std", "kij_test","abs_error","components"]
-    data = data.sort_values(by=["kij_test"],ascending=False)
-    data['error_stats'] = err_stat
-    data['comp_single'] = comp_half
-    data['kij-kji'] = diff_error
-    data['kij-kji.pred_mean'] = err_avg
-    data.to_csv(r'./output/'+name+'.csv', index=True, sep=';')
-    return err_avg
 
-def AARD(y_true,y_pred):
-    from Dataset import num_targets
-    AARD = []
-    RMSE = []
-    for k in range(num_targets):
-        AARD_temp =[]
-        RMSE_temp= []
-        for i in range(len(y_true)):
-            AARD_temp.append(100*abs(1-y_pred[i,k]/y_true[i,k]))
-            RMSE_temp.append((y_pred[i,k]-y_true[i,k])**2)
-        AARD.append(np.mean(AARD_temp).round(2))
-        RMSE.append(((np.mean(RMSE_temp))**0.5).round(2))
-    return AARD, RMSE
-
-def result_export_nonsymmetric(mean_train,std_train,y_test,error, comp, name):
-    from Dataset import num_targets
-    from data_extraction import AARD
-    AARD, RMSE = AARD(y_test,mean_train)
-    mean_train = pd.DataFrame(mean_train)
-    std_train = pd.DataFrame(std_train)
-    y_test = pd.DataFrame(y_test)
-    error=pd.DataFrame(error)
-    correct_side_dev = 0
-    for i in range(error.shape[0]):
-        if error.iloc[i,0]*error.iloc[i,1]<0 and error.iloc[i,0]*error.iloc[i,2]<0:
-            correct_side_dev += 1
-    err_stat = []
-    if num_targets ==1:
-        err_2 = error_threshold(error, 0.02)
-        err_1 = error_threshold(error, 0.01)
-        err_stat = [err_2, err_1]
-    for i in range(num_targets):
-        err_stat.append(np.mean(error.iloc[:,i]))
-    err_stat.append(AARD)
-    err_stat.append(RMSE)
-    err_stat.append(correct_side_dev)
-    for i in range(len(error)-len(err_stat)):
-        err_stat.append('')
-    comp = pd.DataFrame(comp)
-    data=pd.concat([mean_train,std_train,y_test, error,comp],axis=1)
-    if num_targets==1:
-        data.columns=["kij_pred_µ","kij_pred_std", "kij_test","abs_error","components"]
-        data = data.sort_values(by=["abs_error"],ascending=False)
-    elif num_targets==3:
-        data.columns = ["seg_num_pred", "seg_dia_pred", "disp_energy_pred", "seg_num","seg_dia","disp_energy","err1","err2","err3","components"]
-    data['error_stats'] = err_stat
-    data.to_csv(r'./output/'+name+'.csv', index=True, sep=';')
-    return AARD,RMSE
-
-
-def error_threshold(error,limit):
-    k=0
-    for i in error:
-        if i > limit:
-            k+=1
-    perc = (1-k/len(error))*100
-    return perc
-
+    data=pd.concat([mean_train,std_train,y_test, error],axis=1)
+    data.to_csv(r'./output/'+name+'.csv', index=True, header=["kij_pred_µ","kij_pred_std", "kij_test","abs_error"], sep=';')
--- gauss_test/main.py
+++ gauss_test/main.py
@@ -1,288 +1,135 @@
 import pickle
-import torch
-import torchvision
-from copy import copy
 from pathlib import Path
 from torch.utils.data import DataLoader
 from Dataset import *
 from Model import *
 from data_extraction import *
-import random
-
-def main(rand_seed,lr,wd):
-
-    torch.manual_seed(2)
-    X_train, X_test, y_train, y_test, comp_train, comp_test, feature_loss,scale_param, y_test_pre,y_train_pre = load_raw_data(rand_seed)
-    X_test[:,11:] = X_test[:,11:].round(0)
-    X_train[:, 11:] = X_train[:, 11:].round(0)
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(torch.cuda.is_available())
-    build_dir = Path("build")
-    if not build_dir.exists():
-        build_dir.mkdir()
-
-    model_pLV = loss_Model().to(device)
-    model_pLV.load_state_dict(torch.load('./loss_model/state_dict_pLV.pt'))
-    model_pLV.eval()
-    scaler_pLV = torch.tensor(pd.read_csv("./loss_model/scaler_parameters_pLV.csv",sep=";",header=None).to_numpy()).to(device)
-
-
-    model_rho = loss_Model().to(device)
-    model_rho.load_state_dict(torch.load('./loss_model/state_dict_rho.pt'))
-    model_rho.eval()
-    scaler_rho = torch.tensor(pd.read_csv("./loss_model/scaler_parameters_rho.csv",sep=";",header=None).to_numpy()).to(device)
-    scaler_loss = torch.cat((scaler_rho,scaler_pLV))
 
 
 
-    train_dataset = Dataset_input(X_train,y_train,comp_train)
-    test_dataset = Dataset_input(X_test,y_test,comp_test)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+build_dir = Path("build")
+if not build_dir.exists():
+    build_dir.mkdir()
+
+train_fraction = 0.8
+dataset = NoiseDataset()
+
+train_size = int(train_fraction * len(dataset))
+test_size = len(dataset) - train_size
+torch.manual_seed(2)
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+test_dataset = val_dataset
 
 
+loader_kwargs = {
+    "batch_size": 64,
+    "drop_last": False,
+    "num_workers": 0,
+    "pin_memory": False,
+    "persistent_workers": False,
+}
+train_loader = DataLoader(dataset=train_dataset,
+                          **loader_kwargs
+                          )
+val_loader = DataLoader(dataset=val_dataset,
+                        **loader_kwargs
+                        )
+test_loader = DataLoader(dataset=test_dataset,
+                         **loader_kwargs
+                         )
 
-    #print(torch.from_numpy(np.array([1.2,1.0,0.1])).to(device))
-    loader_kwargs = {
-        "batch_size": 64,
-        "drop_last": False,
-        "num_workers": 0,
-        "pin_memory": False,
-        "persistent_workers": False,
-        "shuffle":False
-    }
-    train_loader = DataLoader(dataset=train_dataset,
-                              **loader_kwargs
-                              )
-    test_loader = DataLoader(dataset=test_dataset,
-                             **loader_kwargs
-                             )
+model_path = build_dir / "nn.torch"
+model = Model().to(device)
+model.apply(weights_init_uniform_rule)
+model.to(device)
 
+if model_path.exists():
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+else:
+    loss_fn = torch.nn.GaussianNLLLoss(eps=1e-8)
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 weight_decay=0,
+                                 lr=0.0005
+                                 )
 
+    epochs = 2000
+    start_var = 1000
 
-    model_path = build_dir / "nn.torch"
-    model = Model().to(device)
-    model.apply(weights_init_uniform_rule)
-    model.to(device)
-
-    if model_path.exists():
-        model.load_state_dict(torch.load(model_path))
-        model.eval()
-    else:
-        loss_fn = torch.nn.GaussianNLLLoss(eps=1e-12)
-        optimizer = torch.optim.Adam(model.parameters(),
-                                     weight_decay=wd,  #6e-6
-                                     lr=lr  #0.0048
-                                     )
-        from Dataset import input_length, n_samples, symm_mode, bin_mode, num_targets, std_eval
-        epochs = 10000
-        start_var = 10000
-        loss_min = 0.5
-        iter_best = "none"
-        loss_history = {"train": [], "validation": []}
-
-        for t in range(epochs):
-            print(
-                f"\nEpoch {t + 1}/{epochs}"
-                "\n-------------------------------"
-                f"\ntrain (seed:{seed}):"
-            )
-            if t < start_var:
-                train(train_loader, model, loss_fn, optimizer, scale_param,model_pLV,model_rho,scaler_loss, loss_history["train"],fit_variance=False)
-            else:
-                train(train_loader, model, loss_fn, optimizer, scale_param,model_pLV,model_rho,scaler_loss,loss_history["train"],fit_variance=True)
-
-            print("validate:")
-            if t < start_var:
-                loss = validate(test_loader, model, loss_fn,model_pLV, model_rho, scaler_loss, scale_param,loss_history["validation"], fit_variance=False)
-            else:
-                loss = validate(test_loader, model, loss_fn, model_pLV, model_rho, scaler_loss, scale_param,loss_history["validation"], fit_variance=True)
-            print(f"loss: {loss:.5f}")
-            if loss < loss_min:
-                loss_min = loss.item()
-                iter_best = t
-            if loss < 0.002: #0.0013
-                break
-            elif loss_min >= 2 and t > 600:
-                break
-
-        visualize_training(
-            loss_history["train"],
-            loss_history["validation"],
-            build_dir / "loss.png",
+    loss_history = {"train": [], "validation": []}
+    for t in range(epochs):
+        print(
+            f"\nEpoch {t + 1}/{epochs}"
+            "\n-------------------------------"
+            "\ntrain:"
         )
-        with open(build_dir / "loss.pickle", "wb") as f:
-            pickle.dump(loss_history, f)
-        # torch.save(model.state_dict(), model_path)
-        torch.save(model,
-                   './build/model_gauss')
-
-        print("\nTraining done!")
-        print(loss_min)
-        print(iter_best)
-
-    def calc_pred(data_loader, model,model_rho,model_pLV):
-        model.eval()
-        mean = []
-        std = []
-        y = []
-        for Xi, yi in data_loader:
-            if symm_mode == 1:
-                Xi = Xi.to(device)
-                yi = yi.to(device)
-            elif symm_mode == 2:
-                Xi = Xi.to(device).reshape(-1, input_length)
-                yi = yi.to(device).reshape(-1, 1)
-
-            pred = model(Xi.to(device))  ### shape = (num_targets*2 für std_eval)
-            pred_res = pred.cpu().detach().numpy()
-            loss_pc_rho, loss_pc_pLV = loss_PCSAFT(yi,pred,model_pLV,model_rho,scaler_loss,scale_param)
-            num_targets = yi.size(1)
-            num_outputs = pred.size(1)
-
-            if num_targets == num_outputs:
-                mean.extend(pred_res.tolist())
-                y.extend(yi.cpu().detach().numpy().tolist())
-            else:
-                mean.extend(pred_res[:, 0:(num_outputs - 1)].tolist())
-                y.extend(yi.cpu().detach().numpy().tolist())
-                std.extend(pred_res[:, -num_outputs:])
-            print(f"PC-SAFT loss (Rho): {loss_pc_rho:.2f} ; PC-SAFT loss (pLV): {loss_pc_pLV:.2f}")
-        mean = retransform(mean)
-        y = retransform(y)
-        abs_error = []
-        diff_error = []
-        for i in range(len(y)):
-            abs_error.append(mean[i] - y[i]) #oder abs für korrekten MAE
-        if symm_mode == 2:
-            for l in range(int(len(mean) / 2)):
-                diff_error.append(abs(mean[2 * l] - mean[2 * l + 1]))
-        return mean, std, y, abs_error, diff_error,loss_pc_pLV.cpu().detach().numpy(),loss_pc_rho.cpu().detach().numpy()
-
-
-    pred_test_mean, pred_test_std, y_test, abs_error_test, diff_error_test,loss_pc_pLV_test,loss_pc_rho_test = calc_pred(test_loader, model, model_rho,model_pLV)
-    pred_train_mean, pred_train_std, y_train, abs_error_train, diff_error_train,loss_pc_pLV_train,loss_pc_rho_train = calc_pred(train_loader, model,model_rho,model_pLV)
-
-
-
-    #print(pred_train_mean)
-    # print(pred_train_std)
-    #print(y_train)
-
-    #print(pred_test_mean)
-    # print(pred_test_std)
-    #print(y_test)
-
-    if (len(pred_test_mean) + len(pred_train_mean)) != n_samples:
-        err_avg_train = result_export(pred_train_mean,
-                                      pred_train_std,
-                                      y_train,
-                                      abs_error_train,
-                                      diff_error_train,
-                                      comp_train,
-                                      'output_train')
-        err_avg_test = result_export(pred_test_mean,
-                                     pred_test_std,
-                                     y_test,
-                                     abs_error_test,
-                                     diff_error_test,
-                                     comp_test,
-                                     'output_test')
-    else:
-        AARD_train,RMSE_train = result_export_nonsymmetric(pred_train_mean,
-                                                   pred_train_std,
-                                                   y_train,
-                                                   abs_error_train,
-                                                   comp_train,
-                                                   'output_train')
-        AARD_test,RMSE_test = result_export_nonsymmetric(pred_test_mean,
-                                                  pred_test_std,
-                                                  y_test,
-                                                  abs_error_test,
-                                                  comp_test,
-                                                  'output_test')
-
-    plot_training(y_train,
-                  pred_train_mean,
-                  y_test,
-                  pred_test_mean,
-                  )
-    if bin_mode == 2:
-        plot_error(abs_error_train,
-                   pred_train_std,
-                   abs_error_test,
-                   pred_test_std,
-                   diff_error_train,
-                   err_avg_train,
-                   diff_error_test,
-                   err_avg_test
-                   )
-    return loss_min,iter_best,AARD_test,RMSE_test,feature_loss,loss_pc_pLV_test,loss_pc_rho_test
-
-
-loss_threshold = 0.1
-steps = 1
-steps_seed = 1000
-seed_start = 1001
-seed_save=[]
-loss_min_save=[]
-iter_best_save=[]
-AARD_save=[]
-RMSE_save=[]
-loss_pc_rho_save = []
-loss_pc_pLV_save = []
-lr_save=[]
-wd_save=[]
-feature_loss_save=[]
-seed_list = [seed_start+steps for steps in range(steps_seed)]
-lr_base= 2.8e-5#0.00033
-wd_base = 3.75e-5#2.74e-5
-
-for i in range(steps):
-    lr_temp = lr_base
-    wd_temp = wd_base
-    #lr_temp = round(lr_base + random.uniform(-2e-5,5e-5),7)
-    #wd_temp = round(wd_base + random.uniform(-2e-5,4.5e-5),7)
-    print(lr_temp,wd_temp)
-    k=0
-    for seed in seed_list:
-        k+=1
-        if (lr_temp,wd_temp) not in list(zip(lr_save,wd_save)) or seed_list[0] != seed:
-        #if 0.0 == 0.0:
-            print(f"------------------------------\nstart calculation for seed:{seed}")
-            print(f"lr used:{lr_temp}; wd used: {wd_temp}")
-            loss_min_i, iter_best_i, AARD_test_i, RMSE_test_i, feature_loss_i,loss_pc_pLV_test,loss_pc_rho_test = main(seed, lr_temp, wd_temp)
-            print(
-                f"----------------------------\n{i * len(seed_list) + k}/{len(seed_list) * steps} combinations checked\n"
-                f"Approximated calculation time left: {round((len(seed_list) * steps - (i * len(seed_list) + k)) * 0.9, 0)} min")
-
-            if loss_min_i < loss_threshold:
-                seed_save.append(seed)
-                iter_best_save.append(iter_best_i)
-                loss_min_save.append(loss_min_i)
-                lr_save.append(lr_temp)
-                wd_save.append(wd_temp)
-                AARD_save.append(AARD_test_i)
-                RMSE_save.append(RMSE_test_i)
-                loss_pc_rho_save.append(loss_pc_rho_test)
-                loss_pc_pLV_save.append(loss_pc_pLV_test)
-                feature_loss_save.append(feature_loss_i)
-
-            #try:
-            #    print(f"best_loss: {min(loss_min_save):.6f}\n"
-            #          f"seeds found: {len(loss_min_save)}\n------------------------------------")
-            #except:
-            #    continue
+        if t < start_var:
+            train(train_loader, model, loss_fn, optimizer, loss_history["train"], fit_variance=False)
         else:
-            print(f"lr:{lr_temp}; wd: {wd_temp} already used!")
-            continue
-        try:
-            print(f"best_loss: {round(min(loss_min_save), 6)}\n"
-                  f"seeds found: {len(loss_min_save)}\n------------------------------------")
-        except:
-            print("no loss_min found yet!")
-            pass
+            train(train_loader, model, loss_fn, optimizer, loss_history["train"], fit_variance=True)
 
-print(loss_min_save,seed_save)
-export = pd.DataFrame(data=np.array([seed_save,loss_min_save,iter_best_save,lr_save,wd_save,AARD_save,RMSE_save,feature_loss_save,loss_pc_rho_save,loss_pc_pLV_save],dtype=object),
-                      index=["seed","loss","iter_best","lr","wd","AARD","RMSE","len_X","AARD_rho","RMSE_plv"]).transpose()
-if steps != 1 or len(seed_list) !=1:
-    export.to_csv('./output/optim.csv',sep=";")
+        print("validate:")
+        if t < start_var:
+            validate(val_loader, model, loss_fn, loss_history["validation"], fit_variance=False)
+        else:
+            validate(val_loader, model, loss_fn, loss_history["validation"], fit_variance=True)
+
+    visualize_training(
+        loss_history["train"],
+        loss_history["validation"],
+        build_dir / "loss.png",
+    )
+    with open(build_dir / "loss.pickle", "wb") as f:
+        pickle.dump(loss_history, f)
+    #torch.save(model.state_dict(), model_path)
+    torch.save(model,
+               './build/model_gauss')
+
+    print("\nTraining done!")
+
+
+def calc_pred(data_loader, model):
+    mean = []
+    std = []
+    y = []
+    abs_error=[]
+    for Xi, yi in data_loader:
+        pred = model(Xi.to(device))
+        pred_res = pred.cpu().detach().numpy()
+        abs_error.extend(abs(pred_res[:, 0] - yi.cpu().detach().numpy()[:,0]))
+        mean.extend(pred_res[:, 0])
+        std.extend(pred_res[:, 1] ** 0.5)
+        true_res = yi.cpu().detach().numpy()
+        true_res = true_res.round(6)
+        y.extend(true_res[:, 0].round(6))
+    return mean,std,y, abs_error
+
+pred_train_mean,pred_train_std,y_train, abs_error_train = calc_pred(train_loader,model)
+pred_test_mean,pred_test_std,y_test, abs_error_test = calc_pred(test_loader,model)
+
+print(pred_train_mean)
+print(pred_train_std)
+print(y_train)
+
+print(pred_test_mean)
+print(pred_test_std)
+print(y_test)
+
+result_export(pred_test_mean,
+              pred_test_std,
+              y_test,
+              abs_error_test,
+              'output_test'
+              )
+result_export(pred_train_mean,
+              pred_train_std,
+              y_train,
+              abs_error_train,
+              'output_train'
+              )
+plot_training(y_train,
+              pred_train_mean,
+              y_test,
+              pred_test_mean,
+              )
